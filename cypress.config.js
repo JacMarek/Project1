848954ed34d792f39456e7d7e3ad6d889785f0e9
Jacek Marek <jacek.marek@itmagination.com>
2023-02-03 12:28:39 +0100

    Initial commit of automation Project1 in Cypress

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920, //rozdzielczość przeglądarki do testów
+  e2e: {
+    baseUrl: "https://webdriveruniversity.com",
+    includeShadowDom: true, //to się może kiedyś przydać ale nie wiem do czego
+    chromeWebSecurity: true, //to jest konieczne do przechodzenia pomiędzy hostami, np. z local host do sieci
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}', //to dodaje możliwość m.in. bardziej tradycyjnego zapisu plików z testami - spec.js (nowszy to cy.js)
+    excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*'] //wyklucza z runnera foldery z przykładowymi testami cypress
+    },
+});
---
+++ cypress/e2e/firstSuite.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe("First suite", () => {
+   
+    it("Open home page", () => {
+        cy.visit("/")
+    })
+    
+})
